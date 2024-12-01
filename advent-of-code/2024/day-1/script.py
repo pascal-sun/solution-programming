b9e2636cae63747a4d0c8fb47a44f3f8c522ffb3
Pascal SUN <35567930+pascal-sun@users.noreply.github.com>
2024-12-01 09:59:31 +0100

    feat: 2024 day 1

---
+++ advent-of-code/2024/day-1/script.py
@@ -0,0 +1,33 @@
+from collections import Counter
+from pathlib import Path
+
+
+# Part 1
+def get_total_distance(left_list: list[int], right_list: list[int]) -> int:
+    return sum(abs(left - right) for left, right in zip(sorted(left_list), sorted(right_list)))
+
+
+# Part 2
+def get_similarity_score(left_list: list[int], right_list: list[int]) -> int:
+    counter = Counter(right_list)
+    return sum(left * counter[left] for left in left_list)
+
+
+def main():
+    # Parsing
+    data = Path("input").read_text(encoding="utf-8")
+    left_list = []
+    right_list = []
+    for line in data.split("\n"):
+        left, right = map(int, line.split("   "))
+        left_list.append(left)
+        right_list.append(right)
+
+    # Part 1
+    print(f"Part 1:{get_total_distance(left_list, right_list)}")
+    # Part 2
+    print(f"Part 2:{get_similarity_score(left_list, right_list)}")
+
+
+if __name__ == "__main__":
+    main()
